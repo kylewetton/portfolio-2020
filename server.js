e31d56c3cf9ac354230ec385a621ce45d878156d
kylewetton <kylewetton@me.com>
2020-11-02 09:06:58 +1300

    Fix PDF link

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const compression = require('compression');
 const { body, validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
 const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
 const STATIC = path.resolve(__dirname, 'public');
@@ -22,13 +23,13 @@ const app = express();
  * Pre-middleware routes
  */
 
-app.get('/robots.txt', function (req, res) {
+app.get('/robots.txt', (req, res) => {
   res.type('text/plain');
   res.end();
 });
 
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz';
+app.get('*.js', (req, res, next) => {
+  req.url += '.gz';
   res.set('Content-Encoding', 'gzip');
   res.set('Content-Type', 'text/javascript');
   next();
@@ -40,48 +41,47 @@ app.get('*.js', function (req, res, next) {
 
 app.use(express.static(STATIC));
 app.use(compression({
-  filter: function () { return true; }
+  filter() { return true; },
 }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * Routes
  */
 
-
-app.get('/invoke/home', function(req, res) {
-  fs.readFile(__dirname + '/views/home.html', 'utf8', function (err, html) {
+app.get('/invoke/home', (req, res) => {
+  fs.readFile(`${__dirname}/views/home.html`, 'utf8', (err, html) => {
     res.send(html);
   });
 });
 
-app.get('/invoke/projects', function(req, res) {
-  fs.readFile(__dirname + '/views/projects.html', 'utf8', function (err, html) {
+app.get('/invoke/projects', (req, res) => {
+  fs.readFile(`${__dirname}/views/projects.html`, 'utf8', (err, html) => {
     res.send(html);
   });
 });
 
-app.get('/invoke/about-me', function(req, res) {
-  fs.readFile(__dirname + '/views/about-me.html', 'utf8', function (err, html) {
+app.get('/invoke/about-me', (req, res) => {
+  fs.readFile(`${__dirname}/views/about-me.html`, 'utf8', (err, html) => {
     res.send(html);
   });
 });
 
-app.get('/invoke/contact-me', function(req, res) {
-  fs.readFile(__dirname + '/views/contact-me.html', 'utf8', function (err, html) {
+app.get('/invoke/contact-me', (req, res) => {
+  fs.readFile(`${__dirname}/views/contact-me.html`, 'utf8', (err, html) => {
     res.send(html);
   });
 });
 
-app.get('/invoke/*', function(req, res) {
-  fs.readFile(__dirname + '/views/404.html', 'utf8', function (err, html) {
+app.get('/invoke/*', (req, res) => {
+  fs.readFile(`${__dirname}/views/404.html`, 'utf8', (err, html) => {
     res.send(html);
   });
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 /**
@@ -90,34 +90,33 @@ app.get('*', function(req, res) {
 
 app.post('/send-mail', [
   body('femail')
-  .isEmail()
-  .normalizeEmail(),
+    .isEmail()
+    .normalizeEmail(),
   body('fname')
-  .not().isEmpty()
-  .trim()
-  .escape(),
+    .not().isEmpty()
+    .trim()
+    .escape(),
   body('fmessage')
-  .not().isEmpty()
-  .trim()
-  .escape()
+    .not().isEmpty()
+    .trim()
+    .escape(),
 ], (req, res) => {
-
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const {fname, femail, fmessage} = req.body;
+  const { fname, femail, fmessage } = req.body;
 
-    const transporter = nodemailer.createTransport({
-      service: 'Mailjet',
-      auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS
-      }
-    });
+  const transporter = nodemailer.createTransport({
+    service: 'Mailjet',
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
 
-    var message = {
+  const message = {
     from: '"Kyle Wetton" <noreply@kwetton.dev>',
     to: 'kylewetton@me.com',
     subject: 'Portfolio message',
@@ -125,20 +124,20 @@ app.post('/send-mail', [
       <p>${fname}</p>
       <p>${femail}</p>
       <p>${fmessage}</p>
-    `
+    `,
   };
-  transporter.sendMail(message, function(error, info){
-    if(error){
+  transporter.sendMail(message, (error, info) => {
+    if (error) {
       console.log('Error', error);
-      res.status(503).json({errors: error});
-    }else{
+      res.status(503).json({ errors: error });
+    } else {
       console.log(info);
-      res.json({success: info.response});
-    };
+      res.json({ success: info.response });
+    }
   });
 });
 
 // Start server
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log('Server up and running on ', `http://localhost:${PORT}/`);
 });
